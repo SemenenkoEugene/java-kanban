cbaaee6513b04d8df19a75acb164ca629fade36e
SemenenkoEugene <gks-08@mail.ru>
2023-04-10 13:26:06 +0300

    Sprint 8

--- src/adapter/LocalDateTimeAdapter.java
+++ src/adapter/LocalDateTimeAdapter.java
@@ -14,6 +14,10 @@ public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
 
     @Override
     public void write(JsonWriter jsonWriter, LocalDateTime localDateTime) throws IOException {
+        if (localDateTime==null){
+            jsonWriter.nullValue();
+            return;
+        }
         jsonWriter.value(formatter.format(localDateTime));
     }
 
