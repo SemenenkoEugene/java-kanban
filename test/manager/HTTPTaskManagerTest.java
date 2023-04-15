7cb3bf814b1f39f339ec125eb77ac80191a17ff7
SemenenkoEugene <gks-08@mail.ru>
2023-04-15 19:59:51 +0300

    Sprint 8 fix

--- test/manager/HTTPTaskManagerTest.java
+++ test/manager/HTTPTaskManagerTest.java
@@ -1,14 +1,22 @@
 package manager;
 
+import entity.Status;
+import entity.Task;
 import http.KVServer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
 
 class HTTPTaskManagerTest extends FileBackedTasksManagerTest {
 
-    private  KVServer server;
+    private KVServer server;
 
     @BeforeEach
     public void createManager() {
@@ -26,5 +34,21 @@ class HTTPTaskManagerTest extends FileBackedTasksManagerTest {
     @AfterEach
     public void stopServer() {
         server.stop();
+    }
+
+    @Test
+    public void loadTest() {
+        Task task1 = new Task("Task1", "Description1",
+                Status.NEW, LocalDateTime.of(2023, 4, 15, 19, 40), 15);
+        Task task2 = new Task("Task2", "Description2",
+                Status.NEW, LocalDateTime.of(2023, 4, 15, 20, 40), 15);
+        manager.createTask(task1);
+        manager.createTask(task2);
+        manager.getTaskById(task1.getId());
+        manager.getTaskById(task2.getId());
+        List<Task> history = manager.getHistory();
+
+        assertArrayEquals(List.of(task1, task2).toArray(), history.toArray());
+
     }
 }
